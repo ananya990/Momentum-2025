0db601e788ddbe0bb69e4ecd9d14776cc2c5974b
ananya990 <ananya10v.a@gmail.com>
2025-09-07 22:22:05 +0530

    hero section in progress - parallax left

--- src/app/components/homepage/Hero.jsx
+++ src/app/components/homepage/Hero.jsx
@@ -1,52 +1,82 @@
 import Image from 'next/image'
 import React from 'react'
 
+
 function Hero() {
+  // const layers= new Array.fill(7);
+
   return (
 <>
-    <div className="h-full w-full bg-red-400 flex flex-col items-end ">
-        {/* <Image src="/hero/Layer7.svg" 
-        alt=""
-          height={700}
-          width={700}
-          className="min-w-screen absolute"
-        /> */}
-        <Image src="/hero/Layer6.svg" 
-        alt=""
-          height={700}
-          width={700}
-          className="min-w-screen absolute align-end"
-        />
-        <Image src="/hero/Layer5.svg" 
-        alt=""
-          height={700}
-          width={700}
-          className="min-w-screen absolute"
-        />
-        <Image src="/hero/Layer4.svg" 
-        alt=""
-          height={700}
-          width={700}
-          className="min-w-screen absolute"
-        />
-        <Image src="/hero/Layer3.svg" 
-        alt=""
-          height={700}
-          width={700}
-          className="min-w-screen absolute"
-        />
-        <Image src="/hero/Layer2.svg" 
-        alt=""
-          height={700}
-          width={700}
-          className="min-w-screen absolute"
-        />
-        <Image src="/hero/Layer1.svg" 
-        alt=""
-          height={700}
-          width={700}
-          className="w-full absolute"
-        />
+    <div className="h-full w-full bg-[#FCE6CE] flex flex-col ">
+      {/* {
+        layers.map(()=>{
+
+        })
+      } */}
+        <div className='w-full h-full layer flex items-end'>
+          <Image
+            src="/hero/Layer7.svg"
+            height={100}
+            width={100}
+            alt=''
+            className='w-full'
+          />
+        </div>
+        <div className='w-full h-full absolute flex items-end'>
+          <Image
+            src="/hero/Layer6.svg"
+            height={100}
+            width={100}
+            alt=''
+            className='w-full'
+          />
+        </div>
+        <div className='w-full h-full absolute flex items-end'>
+          <Image
+            src="/hero/Layer5.svg"
+            height={100}
+            width={100}
+            alt=''
+            className='w-full'
+          />
+        </div>
+        <div className='w-full h-full absolute flex items-end'>
+          <Image
+            src="/hero/Layer4.svg"
+            height={100}
+            width={100}
+            alt=''
+            className='w-full'
+          />
+        </div>
+        <div className='w-full h-full absolute flex items-end'>
+          <Image
+            src="/hero/Layer3.svg"
+            height={100}
+            width={100}
+            alt=''
+            className='w-full'
+          />
+        </div>
+        <div className='w-full h-full absolute flex items-end'>
+          <Image
+            src="/hero/Layer2.svg"
+            height={100}
+            width={100}
+            alt=''
+            className='w-full'
+          />
+        </div>
+        <div className='w-full h-full absolute flex items-end'>
+          <Image
+            src="/hero/Layer1.svg"
+            height={100}
+            width={100}
+            alt=''
+            className='w-full'
+          />
+        </div>
+        
     </div>
 </>
   )
