4d3a982f03f70dc5f9b0834ffd82f00c33363a2a
ananya990”;GIT_AUTHOR_EMAIL=ananya10v.a@gmail.com” <142164935+ananya990@users.noreply.github.com>
2025-09-07 20:50:51 +0530

    hero section in progress

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
 import Image from "next/image";
-
+import Hero from "./components/homepage/Hero.jsx"
 export default function Home() {
   return (
-    <></>
+    <>
+      <Hero/>
+    </>
   );
 }
