7374b30faa14490298b5a9cf6ad36ddb6993aa40
ananya990 <ananya10v.a@gmail.com>
2025-09-08 22:58:14 +0530

    minor changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,20 @@
 import Image from "next/image";
 import Hero from "./components/homepage/Hero.jsx"
 import About from "./components/homepage/About.jsx"
+import TopEvents from "./components/homepage/TopEvents.jsx";
+import Sponsors from "./components/homepage/Sponsors.jsx";
+import Faq from "./components/homepage/Faq.jsx";
+import Footer from "./components/homepage/Footer.jsx";
 
 export default function Home() {
   return (
     <>
       <Hero/>
       <About/>
+      <TopEvents/>
+      <Sponsors/>
+      <Faq/>
+      <Footer/>
     </>
   );
 }
