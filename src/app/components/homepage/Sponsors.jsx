b49bdc1a3992db7bf034a3695f8406d4ad53358f
Lakshay Yadav <lionleo110@gmail.com>
2025-09-09 01:17:23 +0530

    sponsors added

--- src/app/components/homepage/Sponsors.jsx
+++ src/app/components/homepage/Sponsors.jsx
@@ -1,10 +1,18 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Sponsors() {
   return (
     <>
-      <div>
-        
+      <div className='flex flex-col space-y-5 items-center '>
+        <div className='text-white font-bold text-4xl'>Our Sponsors</div>
+        <Image
+          src="/sponsors/Sponsors.svg"
+          height={20}
+          width={20}
+          alt=""
+          className="w-full min-h-auto"
+        />
       </div>
     </>
   )
