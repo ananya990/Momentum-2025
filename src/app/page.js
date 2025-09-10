2b5eb9fa83e65948debe58a32da505f0c8490d2b
ananya990 <ananya10v.a@gmail.com>
2025-09-10 23:15:21 +0530

    about section responive

--- src/app/page.js
+++ src/app/page.js
@@ -4,15 +4,13 @@ import TopEvents from "./components/homepage/TopEvents.jsx";
 import Sponsors from "./components/homepage/Sponsors.jsx";
 import Faqs from "./components/homepage/Faqs.jsx";
 import Footer from "./components/homepage/Footer.jsx";
-import NavBar from "./components/homepage/NavBar.jsx";
 import FooterMain from "./components/homepage/FooterMain.jsx";
 
 export default function Home() {
   return (
     <>
-      <NavBar/>
       <Hero/>
-      {/* <About/> */}
+      <About/>
       <TopEvents/>
       <Sponsors/>
       <Faqs/>
