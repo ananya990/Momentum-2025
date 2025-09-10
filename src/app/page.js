ac36fd2e813f35ff7851c14413f130be90a7ffb0
ananya990 <ananya10v.a@gmail.com>
2025-09-10 22:35:04 +0530

    footer responsiveness done

--- src/app/page.js
+++ src/app/page.js
@@ -4,16 +4,18 @@ import TopEvents from "./components/homepage/TopEvents.jsx";
 import Sponsors from "./components/homepage/Sponsors.jsx";
 import Faqs from "./components/homepage/Faqs.jsx";
 import Footer from "./components/homepage/Footer.jsx";
+import FooterMain from "./components/homepage/FooterMain.jsx";
 
 export default function Home() {
   return (
     <>
       <Hero/>
-      <About/>
+      {/* <About/> */}
       <TopEvents/>
       <Sponsors/>
       <Faqs/>
-      <Footer/>
+      <FooterMain/>
+      {/* <Footer/> */}
     </>
   );
 }
