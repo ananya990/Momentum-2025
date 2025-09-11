bf6d4a28d70d9a9f83218c93174a4db7239a95d8
ananya990”;GIT_AUTHOR_EMAIL=ananya10v.a@gmail.com” <142164935+ananya990@users.noreply.github.com>
2025-09-11 20:49:55 +0530

    minor changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 import Hero from "./components/homepage/Hero.jsx"
 import About from "./components/homepage/About.jsx"
-import TopEvents from "./components/homepage/TopEvents.jsx";
 import Sponsors from "./components/homepage/Sponsors.jsx";
 import Faqs from "./components/homepage/Faqs.jsx";
 import Footer from "./components/homepage/Footer.jsx";
