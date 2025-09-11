8cb35247af12ee46af0b2a0d638d5f69b813d336
ananya990 <ananya10v.a@gmail.com>
2025-09-11 22:53:22 +0530

    responsiveness improved

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
 import Hero from "./components/homepage/Hero.jsx"
 import About from "./components/homepage/About.jsx"
-import TopEvents from "./components/homepage/TopEvents.jsx";
 import Sponsors from "./components/homepage/Sponsors.jsx";
 import Faqs from "./components/homepage/Faqs.jsx";
 import Footer from "./components/homepage/Footer.jsx";
-import NavBar from "./components/homepage/NavBar.jsx";
 import FeaturedEvents from "./components/homepage/FeaturedEvents.jsx";
 import FooterMain from "./components/homepage/FooterMain.jsx";
 
