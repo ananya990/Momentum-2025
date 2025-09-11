fb10a34c5ff02ae1711b3cc6e67e14af2e026ff1
Lakshay Yadav <lionleo110@gmail.com>
2025-09-12 00:37:09 +0530

    release - 1

---
+++ src/app/components/homepage/EventsMarquee.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Marquee from 'react-fast-marquee'
+
+function EventsMarquee() {
+  return (
+    <>
+        <div
+            className='w-full text-white text-4xl -mt-20 text-center'
+        >Coming Soon</div>
+    </>
+  )
+}
+
+export default EventsMarquee
