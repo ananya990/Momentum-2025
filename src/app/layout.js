012294ab7bf20a066d8d3ff53181dbefa501466f
ananya990 <ananya10v.a@gmail.com>
2025-09-12 00:39:53 +0530

    release - 1

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/globals/NavBar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Momentum 2025",
