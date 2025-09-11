388b49becf2ddf025fde435379362bb5552b71cb
ananya990 <ananya10v.a@gmail.com>
2025-09-11 23:41:56 +0530

    parallax added

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+"use client"
 import Hero from "./components/homepage/Hero.jsx"
 import About from "./components/homepage/About.jsx"
 import Sponsors from "./components/homepage/Sponsors.jsx";
@@ -5,16 +6,33 @@ import Faqs from "./components/homepage/Faqs.jsx";
 import Footer from "./components/homepage/Footer.jsx";
 import FeaturedEvents from "./components/homepage/FeaturedEvents.jsx";
 import FooterMain from "./components/homepage/FooterMain.jsx";
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
+  const [offset, setOffset] = useState(0);
+  
+    useEffect(() => {
+      const handleScroll = () => {
+        setOffset(window.scrollY);
+      };
+      window.addEventListener("scroll", handleScroll);
+      return () => window.removeEventListener("scroll", handleScroll);
+
+  }, []);
+// style={{ transform: `translateY(-${offset * 0.45}px)` }}
   return (
     <>
       <Hero/>
-      <About/>
-      <FeaturedEvents/>
-      <Sponsors/>
-      <Faqs/>
-      <FooterMain/>
+      <div 
+        style={{ transform: `translateY(-${offset * 0.55}px)` }}
+        className="bg-[#5A100B]"
+      >
+        <About/>
+        <FeaturedEvents/>
+        <Sponsors/>
+        <Faqs/>
+        <FooterMain/>
+      </div>
       {/* <Footer/> */}
     </>
   );
