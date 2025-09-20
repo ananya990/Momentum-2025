ef23e4e0a78c5858b74545841ab84cb36dc815a0
ananya990 <ananya10v.a@gmail.com>
2025-09-20 19:58:05 +0530

    events api completed

--- src/app/api/events/route.js
+++ src/app/api/events/route.js
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server";
+import events from "../../../data/events.json"
 
 
 export function GET(){
     try {
-        return NextResponse.json({data: "working fine!"});
+        return NextResponse.json(events);
     } catch (error) {
         return NextResponse.json({msg:"error",errMsg: error});
     }
