32c82685374f1d007c10e0bed4ca16861326dc35
Lakshay Yadav <lionleo110@gmail.com>
2025-10-06 16:19:04 +0530

    glassmorphed navbar added

--- src/app/components/globals/NavBar.jsx
+++ src/app/components/globals/NavBar.jsx
@@ -1,10 +1,28 @@
+"use client"
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function NavBar() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 150) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <>
-        <div className='z-50 flex text-white w-full justify-start px-1 lg:px-6 xl:px-10 py-4 montserrat-light fixed'>
+        <div 
+          className={`z-50 flex text-white w-full justify-start px-1 lg:px-6 xl:px-10 py-3 montserrat-light fixed ${scrolled?"bg-white/40 backdrop-blur-md":""}`}
+        >
             <div className='flex items-center justify-center space-x-4'>
                 <Image src="/navbar/NCU-Logo.svg" alt='NCU Logo' height={70} width={70} className='h-10 w-14' />
                 <Image src="/navbar/MMT-Logo.svg" alt='Momentum Logo' height={50} width={50} className='h-10 w-10' /> 
