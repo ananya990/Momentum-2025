5e7a5e21b261d728c96f64bb83a1e987bc67059e
Lakshay Yadav <lionleo110@gmail.com>
2025-10-14 22:30:00 +0530

    Total Revenue Added

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -11,13 +11,14 @@ function Overview({loading, data}) {
         <>Loading...</>
         :
         <div className='flex justify-between space-x-3'>
-          <div className='h-10 bg-[#3c3c3c] w-full px-5 py-10 text-center rounded-lg'>
-            Total Registrations: {data.payments}
+          <div className=' bg-[#3c3c3c] w-full px-5 py-10 text-center rounded-lg flex flex-col space-y-2 '>
+            <div className='text-xl font-semibold'>Rs. {data.amount}</div>
+            <div>Total Registrations: {data.payments}</div>
           </div>
-          <div className='h-10 bg-[#3c3c3c] w-full px-5 py-10 text-center rounded-lg'>
+          <div className=' bg-[#3c3c3c] w-full px-5 py-10 text-center rounded-lg'>
             Total Teams: {data.teams}
           </div>
-          <div className='h-10 bg-[#3c3c3c] w-full px-5 py-10 text-center rounded-lg'>
+          <div className=' bg-[#3c3c3c] w-full px-5 py-10 text-center rounded-lg'>
             Total Users: {data.users}
           </div>
         </div>
