669e94890e9fb4b56011e3d9a3f55cd28b30700c
Ananya V A <142164935+ananya990@users.noreply.github.com>
2025-11-06 19:35:47 +0530

    Add ticket details

--- src/components/MyEvents.tsx
+++ src/components/MyEvents.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import eve from '../data/events.json';
 
 function MyEvents({events}) {
@@ -12,8 +13,10 @@ function MyEvents({events}) {
                     return(
                       <div 
                         key={index}
-                        className='h-14 border-[1px] border-[#41454d] rounded-md flex items-center px-2 md:px-4 justify-between'>
-                          <div className='max-h-[60%]'>{eve[event].name}</div>
+                        className='min-h-14 border-[1px] border-[#41454d] rounded-md flex flex-col items-start p-2 md:px-4 space-y-1'>
+                          <div className='text-sm font-bold'>{eve[event].name}</div>
+                          <div className='text-xs font-extralight'>{eve[event].dayTime}</div>
+                          <div className='text-xs font-extralight'>Venue: {eve[event].venue}</div>
                       </div>
                     )
                 })
