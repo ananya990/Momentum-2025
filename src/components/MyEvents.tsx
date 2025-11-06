0ea9fb640f17500470e3f5478ce155f36b96c052
Ananya V A <142164935+ananya990@users.noreply.github.com>
2025-11-06 10:39:22 +0530

    Testing profile section

--- src/components/MyEvents.tsx
+++ src/components/MyEvents.tsx
@@ -13,8 +13,12 @@ function MyEvents({events}) {
                     return(
                       <div 
                         key={index}
-                        className='h-14 border-[1px] border-[#41454d] rounded-md flex items-center px-2 md:px-4 justify-between'>
-                          <div className='max-h-[60%]'>{eve[event].name}</div>
+                        className='h-14 border-[1px] border-[#41454d] rounded-md flex flex-col items-center px-2 md:px-4 justify-between space-y-1'>
+                          <div className='flex w-full justify-between'>
+                            <div className='max-h-[60%]'>{eve[event].name}</div>
+                            <div className='max-h-[60%]'>{eve[event].dayTime}</div>
+                          </div>
+                          <div className='max-h-[40%] text-xs'>{eve[event].venue}</div>
                       </div>
                     )
                 })
